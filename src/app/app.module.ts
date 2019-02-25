796e4e821c9ee424d4f1c8fb67329502f026d850
jotiramdevadkar <jotiamdevadkar@gmail.com>
2019-02-25 15:56:14 +0530

    list-user component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AddUserComponent,
-    EditUserComponent
+    EditUserComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule,
